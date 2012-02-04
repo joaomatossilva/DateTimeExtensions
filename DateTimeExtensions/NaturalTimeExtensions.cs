5478e568c81880dcfad88566f22498236b38969b
Joao Silva <kappy@acydburne.com.pt>
2012-02-04 21:02:49 +0000

    Added the extension points on DateTime type

---
+++ DateTimeExtensions/NaturalTimeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DateTimeExtensions {
+	public static class NaturalTimeExtensions {
+
+		public static string ToNaturalText(this DateTime fromDate, DateTime toDate, bool round, INaturalTextCultureInfo cultureInfo) {
+			return cultureInfo.ToNaturalText(toDate.Subtract(fromDate), round);
+		}
+
+		public static string ToNaturalText(this DateTime fromDate, DateTime toDate, bool round) {
+			return ToNaturalText(fromDate, toDate, round, new DateTimeCultureInfo());
+		}
+
+		public static string ToExactNaturalText(this DateTime fromDate, DateTime toDate, INaturalTextCultureInfo cultureInfo) {
+			return cultureInfo.ToExactNaturalText(toDate.Subtract(fromDate));
+		}
+
+		public static string ToExactNaturalText(this DateTime fromDate, DateTime toDate) {
+			return ToExactNaturalText(fromDate, toDate, new DateTimeCultureInfo());
+		}
+	}
+}
