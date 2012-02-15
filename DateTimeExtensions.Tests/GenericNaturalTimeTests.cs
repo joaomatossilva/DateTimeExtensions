a7493b919410a17470a22a75767c1e5ec7739297
Joao Silva <kappy@acydburne.com.pt>
2012-02-15 23:35:35 +0000

    Added round time feature and tests

--- DateTimeExtensions.Tests/GenericNaturalTimeTests.cs
+++ DateTimeExtensions.Tests/GenericNaturalTimeTests.cs
@@ -15,13 +15,25 @@ namespace DateTimeExtensions.Tests {
 		[Test]
 		public void can_tranlate_to_natural_text() {
 			var fromTime = DateTime.Now;
-			var toTime = DateTime.Now.AddHours(2);
+			var toTime = DateTime.Now.AddHours(2).AddMinutes(45);
 
-			var naturalText = fromTime.ToNaturalText(toTime, true, foo_ci);
+			var naturalText = fromTime.ToNaturalText(toTime, false, foo_ci);
 
 			Assert.IsNotNullOrEmpty(naturalText);
 			Assert.AreEqual("2 hours", naturalText);
 		}
+
+		[Test]
+		public void can_tranlate_to_natural_text_rounded() {
+			var fromTime = DateTime.Now;
+			var toTime = fromTime.AddHours(2).AddMinutes(45);
+
+			var naturalText = fromTime.ToNaturalText(toTime, true, foo_ci);
+
+			Assert.IsNotNullOrEmpty(naturalText);
+			Assert.AreEqual("3 hours", naturalText);
+		}
+
 
 		[Test]
 		public void can_tranlate_to_exact_natural_text() {
@@ -44,6 +56,20 @@ namespace DateTimeExtensions.Tests {
 			Assert.IsNotNullOrEmpty(naturalText);
 			Assert.AreEqual("2 years, 2 months, 3 days, 4 hours, 5 minutes, 6 seconds", naturalText);
 		}
+
+		[Test]
+		public void are_orderless() {
+			var fromTime = DateTime.Now;
+			var toTime = fromTime.AddYears(2).AddMonths(2).AddDays(3).AddHours(4).AddMinutes(5).AddSeconds(6);
+
+			var naturalText = fromTime.ToExactNaturalText(toTime, foo_ci);
+			var naturalTextReverse = toTime.ToExactNaturalText(fromTime, foo_ci);
+
+			Assert.IsNotNullOrEmpty(naturalText);
+			Assert.IsNotNullOrEmpty(naturalTextReverse);
+			Assert.AreEqual(naturalTextReverse, naturalText);
+		}
+
 
 		[Test]
 		public void can_pluralize_years() {
@@ -88,7 +114,61 @@ namespace DateTimeExtensions.Tests {
 			Assert.IsNotNullOrEmpty(naturalText_single);
 			Assert.AreEqual("2 days, 2 hours, 2 minutes, 2 seconds", naturalText_plural);
 			Assert.AreEqual("1 day, 1 hour, 1 minute, 1 second", naturalText_single);
-		}	
+		}
+
+
+		[Test]
+		public void can_round() {
+			var fromTime = DateTime.Now;
+			var overflowYear = fromTime.AddYears(1).AddMonths(8);
+			var noOverflowYear = fromTime.AddYears(1).AddMonths(2);
+			Assert.AreEqual("2 years", fromTime.ToNaturalText(overflowYear, foo_ci));
+			Assert.AreEqual("1 year", fromTime.ToNaturalText(noOverflowYear, foo_ci));
+
+			var overflowMonth = fromTime.AddMonths(1).AddDays(20);
+			var noOverflowMonth = fromTime.AddMonths(1).AddDays(10);
+			Assert.AreEqual("2 months", fromTime.ToNaturalText(overflowMonth, foo_ci));
+			Assert.AreEqual("1 month", fromTime.ToNaturalText(noOverflowMonth, foo_ci));
+
+			var overflowDays = fromTime.AddDays(1).AddHours(20);
+			var noOverflowDays = fromTime.AddDays(1).AddHours(10);
+			Assert.AreEqual("2 days", fromTime.ToNaturalText(overflowDays, foo_ci));
+			Assert.AreEqual("1 day", fromTime.ToNaturalText(noOverflowDays, foo_ci));
+
+			var overflowHours = fromTime.AddHours(1).AddMinutes(40);
+			var overflowHours2 = fromTime.AddHours(1).AddMinutes(55);
+			var noOverflowHours = fromTime.AddHours(1).AddMinutes(10);
+			Assert.AreEqual("2 hours", fromTime.ToNaturalText(overflowHours, foo_ci));
+			Assert.AreEqual("2 hours", fromTime.ToNaturalText(overflowHours2, foo_ci));
+			Assert.AreEqual("1 hour", fromTime.ToNaturalText(noOverflowHours, foo_ci));
+
+			var overflowMinutes = fromTime.AddMinutes(1).AddSeconds(40);
+			var overflowMinutes2 = fromTime.AddMinutes(1).AddSeconds(55);
+			var noOverflowMinutes = fromTime.AddMinutes(1).AddSeconds(10);
+			Assert.AreEqual("2 minutes", fromTime.ToNaturalText(overflowMinutes, foo_ci));
+			Assert.AreEqual("2 minutes", fromTime.ToNaturalText(overflowMinutes2, foo_ci));
+			Assert.AreEqual("1 minute", fromTime.ToNaturalText(noOverflowMinutes, foo_ci));
+
+			var roundedMinutes_lowValue = fromTime.AddMinutes(3);
+			Assert.AreEqual("3 minutes", fromTime.ToNaturalText(roundedMinutes_lowValue, foo_ci));
+			
+			var roundedMinutes_firstQuarter1 = fromTime.AddMinutes(21);
+			var roundedMinutes_firstQuarter2= fromTime.AddMinutes(9);
+			Assert.AreEqual("15 minutes", fromTime.ToNaturalText(roundedMinutes_firstQuarter1, foo_ci));
+			Assert.AreEqual("15 minutes", fromTime.ToNaturalText(roundedMinutes_firstQuarter2, foo_ci));
+
+			var roundedMinutes_half1 = fromTime.AddMinutes(25);
+			var roundedMinutes_half2 = fromTime.AddMinutes(35);
+			Assert.AreEqual("30 minutes", fromTime.ToNaturalText(roundedMinutes_half1, foo_ci));
+			Assert.AreEqual("30 minutes", fromTime.ToNaturalText(roundedMinutes_half2, foo_ci));
+			
+			var roundedMinutes_lastQuarter1 = fromTime.AddMinutes(42);
+			var roundedMinutes_lastQuarter2 = fromTime.AddMinutes(47);
+			Assert.AreEqual("45 minutes", fromTime.ToNaturalText(roundedMinutes_lastQuarter1, foo_ci));
+			Assert.AreEqual("45 minutes", fromTime.ToNaturalText(roundedMinutes_lastQuarter2, foo_ci));
+		}
+
+
 
 	}
 }
--- DateTimeExtensions/Strategies/NaturalTimeStrategyBase.cs
+++ DateTimeExtensions/Strategies/NaturalTimeStrategyBase.cs
@@ -18,50 +18,105 @@ namespace DateTimeExtensions.Strategies {
 			}
 		}
 
-		public string ToNaturalText(DateDiff dateDiff, bool round) {
-			return BuildNaturalText(dateDiff, true, round);
+		public virtual string ToNaturalText(DateDiff dateDiff, bool round) {
+			if (dateDiff.Years > 0) {
+				if (round && dateDiff.Months >= 6) {
+					return this.GetYearsText(dateDiff.Years + 1);
+				}
+				return this.GetYearsText(dateDiff.Years);
+			}
+			if (dateDiff.Months > 0) {
+				if (round && dateDiff.Days >= 15) {
+					return this.GetMonthsText(dateDiff.Months + 1);
+				}
+				return this.GetMonthsText(dateDiff.Months);
+			}
+			if (dateDiff.Days > 0) {
+				if (round && dateDiff.Hours >= 12) {
+					return this.GetDaysText(dateDiff.Days + 1);
+				}
+				return this.GetDaysText(dateDiff.Days);
+			}
+			if (!round) {
+				if (dateDiff.Hours > 0) {
+					return this.GetHoursText(dateDiff.Hours);
+				}
+				if (dateDiff.Minutes > 0) {
+					return this.GetMinutesText(dateDiff.Minutes);
+				}
+				if (dateDiff.Seconds > 0) {
+					return this.GetSecondsText(dateDiff.Seconds);
+				}
+			}
+			bool carrier;
+			var minutes = SixtyToQuarters(dateDiff.Minutes, out carrier);
+			if (dateDiff.Hours > 0 || carrier) {
+				if (carrier || minutes >= 30) {
+					return this.GetHoursText(dateDiff.Hours + 1);
+				}
+				return this.GetHoursText(dateDiff.Hours);
+			}
+			var seconds = this.SixtyToQuarters(dateDiff.Seconds, out carrier);
+			if (minutes == 0 && dateDiff.Minutes > 0) {
+				if(seconds >= 30 || carrier) {
+					return this.GetMinutesText(dateDiff.Minutes + 1);
+				}
+				return this.GetMinutesText(dateDiff.Minutes);
+			}
+			if (minutes == 0 && carrier) {
+				return this.GetMinutesText(1);
+			}
+			if (minutes > 0) {
+				return this.GetMinutesText(minutes);
+			}
+			if (seconds > 0) {
+				return this.GetSecondsText(seconds);
+			}
+			return string.Empty;
 		}
 
-		public string ToExactNaturalText(DateDiff dateDiff) {
-			return this.BuildNaturalText(dateDiff, false, false);
-		}
-
-		protected virtual string BuildNaturalText(DateDiff dateDiff, bool stopOnFirsValue, bool round) {
+		public virtual string ToExactNaturalText(DateDiff dateDiff) {
 			string text = string.Empty;
 			if (dateDiff.Years > 0) {
 				text = this.AddSentence(text, this.GetYearsText(dateDiff.Years));
-				if (stopOnFirsValue) {
-					return text;
-				}
 			}
 			if (dateDiff.Months > 0) {
 				text = this.AddSentence(text, this.GetMonthsText(dateDiff.Months));
-				if (stopOnFirsValue) {
-					return text;
-				}
 			}
 			if (dateDiff.Days > 0) {
 				text = this.AddSentence(text, this.GetDaysText(dateDiff.Days));
-				if (stopOnFirsValue) {
-					return text;
-				}
 			}
 			if (dateDiff.Hours > 0) {
 				text = this.AddSentence(text, this.GetHoursText(dateDiff.Hours));
-				if (stopOnFirsValue) {
-					return text;
-				}
 			}
 			if (dateDiff.Minutes > 0) {
 				text = this.AddSentence(text, this.GetMinutesText(dateDiff.Minutes));
-				if (stopOnFirsValue) {
-					return text;
-				}
 			}
 			if (dateDiff.Seconds > 0) {
 				text = this.AddSentence(text, this.GetSecondsText(dateDiff.Seconds));
 			}
 			return text;
+		}
+
+		private int SixtyToQuarters(int sixtyValue, out bool carrier) {
+			if (sixtyValue < 0 || sixtyValue >= 60) {
+				throw new ArgumentException("sixtyValue must be between 0 and 59", "sixtyValue");
+			}
+			carrier = false;
+			if (sixtyValue <= 7) {
+				return 0;
+			}
+			if (sixtyValue <= 22) {
+				return 15;
+			}
+			if (sixtyValue <= 37) {
+				return 30;
+			}
+			if (sixtyValue <= 52) {
+				return 45;
+			}
+			carrier = true;
+			return 0;
 		}
 
 		private string AddSentence(string text, string sentence) {
