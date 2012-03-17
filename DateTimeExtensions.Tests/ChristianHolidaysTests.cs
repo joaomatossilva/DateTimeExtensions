59a3c3ad837e9087acb12d1bd3ef6e91f35b800a
Joao Silva <kappy@acydburne.com.pt>
2012-03-17 18:40:10 +0000

    improved christian mobile holidays tests

--- DateTimeExtensions.Tests/ChristianHolidaysTests.cs
+++ DateTimeExtensions.Tests/ChristianHolidaysTests.cs
@@ -10,20 +10,41 @@ namespace DateTimeExtensions.Tests {
 	[TestFixture]
 	class ChristianHolidaysTests {
 
+		private IEnumerable<int> years;
+
+		private IDictionary<int, DateTime> easterDates;
+
+		[TestFixtureSetUp]
+		void SetUpYears() {
+			years = new int[] {1800, 1899, 1900, 1999, 2000, 2025, 2035, 2067, 2099};
+			easterDates = new Dictionary<int, DateTime> {
+				{ 1800, new DateTime(1800, 4, 13) },
+				{ 1899, new DateTime(1899, 4, 2) },
+				{ 1900, new DateTime(1900, 4, 15) },
+				{ 1999, new DateTime(1999, 4, 4) },
+				{ 2000, new DateTime(2000, 4, 23) },
+				{ 2025, new DateTime(2025, 4, 20) },
+				{ 2035, new DateTime(2035, 3, 25) },
+				{ 2067, new DateTime(2067, 4, 3) },
+				{ 2099, new DateTime(2099, 4, 12) }
+			};
+		}
+
+		[Test]
 		public void can_calculate_easter() {
-			for (int year = 2012; year < 2020; year++) {
+			foreach (int year in years) {
 				var easterHoliday = ChristianHolidays.Easter;
 				var easter = easterHoliday.GetInstance(year);
 				Assert.IsTrue(easter.HasValue);
 				Assert.AreEqual(DayOfWeek.Sunday, easter.Value.DayOfWeek);
+				Assert.AreEqual(easterDates[year], easter.Value);
 			}
 		}
 
 		[Test]
 		public void pentecost_is_49_days_after_easter() {
-			for (int year = 2012; year < 2020; year++) {
-				var easterHoliday = ChristianHolidays.Easter;
-				var easter = easterHoliday.GetInstance(year);
+			foreach (int year in years) {
+				var easter = easterDates[year];
 
 				var pentecostHoliday = ChristianHolidays.Pentecost;
 				var pentecost = pentecostHoliday.GetInstance(year);
@@ -32,15 +53,14 @@ namespace DateTimeExtensions.Tests {
 
 				//source: http://en.wikipedia.org/wiki/Pentecost
 				// According to Christian tradition, Pentecost is always seven weeks after Easter Sunday; that is to say, 50 days after Easter (inclusive of Easter Day)
-				Assert.AreEqual(easter.Value.AddDays(49), pentecost.Value);
+				Assert.AreEqual(easter.AddDays(49), pentecost.Value);
 			}	
 		}
 		
 		[Test]
-		public void ascencion_is_40_days_after_easter() {
-			for (int year = 2012; year < 2020; year++) {
-				var easterHoliday = ChristianHolidays.Easter;
-				var easter = easterHoliday.GetInstance(year);
+		public void ascencion_is_39_days_after_easter() {
+			foreach (int year in years) {
+				var easter = easterDates[year];
 
 				var ascencionHoliday = ChristianHolidays.Ascension;
 				var ascencion = ascencionHoliday.GetInstance(year);
@@ -50,15 +70,14 @@ namespace DateTimeExtensions.Tests {
 				//source: http://en.wikipedia.org/wiki/Ascension_Day
 				// Ascension Day is traditionally celebrated on a Thursday, the fortieth day of Easter
 				// again, easter day is included
-				Assert.AreEqual(easter.Value.AddDays(39), ascencion.Value);
+				Assert.AreEqual(easter.AddDays(39), ascencion.Value);
 			}			
 		}
 
 		[Test]
 		public void palmSunday_is_7_days_before_easter() {
-			for (int year = 2012; year < 2020; year++) {
-				var easterHoliday = ChristianHolidays.Easter;
-				var easter = easterHoliday.GetInstance(year);
+			foreach (int year in years) {
+				var easter = easterDates[year];
 
 				var palmSundayHoliday = ChristianHolidays.PalmSunday;
 				var palmSunday = palmSundayHoliday.GetInstance(year);
@@ -67,7 +86,20 @@ namespace DateTimeExtensions.Tests {
 
 				//source: http://en.wikipedia.org/wiki/Palm_Sunday
 				//Palm Sunday is a Christian moveable feast that falls on the Sunday before Easter
-				Assert.AreEqual(easter.Value.AddDays(-7), palmSunday.Value);
+				Assert.AreEqual(easter.AddDays(-7), palmSunday.Value);
+			}
+		}
+
+		[Test]
+		public void carnival_is_47_days_before_easter() {
+			foreach (int year in years) {
+				var easter = easterDates[year];
+
+				var carnivalHoliday = ChristianHolidays.Carnival;
+				var carnival = carnivalHoliday.GetInstance(year);
+				Assert.IsTrue(carnival.HasValue);
+				Assert.AreEqual(DayOfWeek.Tuesday, carnival.Value.DayOfWeek);
+				Assert.AreEqual(easter.AddDays(-47), carnival.Value);
 			}
 		}
 
