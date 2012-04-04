3bee3b306dc0c4b9e76469e5a10e8cef2b885a9c
Joao Silva <kappy@acydburne.com.pt>
2012-04-04 17:41:05 +0100

    Added Interface for exporting formats and basic Office Holidays File Format

---
+++ DateTimeExtensions/Export/IExportHolidaysFormat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using DateTimeExtensions.Strategies;
+
+namespace DateTimeExtensions.Export {
+	public interface IExportHolidaysFormat {
+		void Export(IHolidayStrategy holidayStrategy, int year, TextWriter writer);
+		void Export(string name, IHolidayStrategy holidayStrategy, int year, TextWriter writer);
+		void Export(DateTimeCultureInfo dateTimeCultureInfo, int year, TextWriter writer);
+	}
+}
---
+++ DateTimeExtensions/Export/OfficeHolidaysExportHolidaysFormat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using DateTimeExtensions.Strategies;
+
+namespace DateTimeExtensions.Export {
+	public class OfficeHolidaysExportHolidaysFormat: IExportHolidaysFormat {
+
+		private const string HolidayLineFormat = "{0}, {1}";
+		private const string HeaderLineFomat = "{0} ###";
+		private const string DateFormat = "yyyy/MM/dd";
+
+		public void Export(string name, IHolidayStrategy holidayStrategy, int year, TextWriter writer) {
+			writer.WriteLine(HeaderLineFomat, name);
+			foreach (var holiday in holidayStrategy.GetHolidaysOfYear(year)) {
+				var instance = holiday.GetInstance(year);
+				if(instance.HasValue) {
+					writer.WriteLine(HolidayLineFormat, holiday.Name, instance.Value.ToString(DateFormat));
+				}
+			}
+		}
+
+		public void Export(IHolidayStrategy holidayStrategy, int year, TextWriter writer) {
+			Export(holidayStrategy.GetType().Name, holidayStrategy, year, writer);
+		}
+
+		public void Export(DateTimeCultureInfo dateTimeCultureInfo, int year, TextWriter writer) {
+			var strategy = dateTimeCultureInfo.LocateHolidayStrategy(dateTimeCultureInfo.Name);
+			Export(dateTimeCultureInfo.Name, strategy, year, writer);
+		}
+	}
+}
