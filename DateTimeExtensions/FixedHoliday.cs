0edd55ec290f4efd4158f92aa7ae0aafe1048126
Joao Matos Silva <kappy@acydburne.com.pt>
2012-04-10 15:33:36 +0100

    Added the Calendar component to FixedHoliday and DayInYear to make them Calendar independent. The Responsability check the year ocurrence and instance was been delegated to DayInYear

--- DateTimeExtensions/FixedHoliday.cs
+++ DateTimeExtensions/FixedHoliday.cs
@@ -1,28 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 
 namespace DateTimeExtensions {
 	public class FixedHoliday : Holiday {
-		private DayInYear day;
+		private readonly DayInYear day;
 
 		public FixedHoliday(string name, DayInYear day)
 			: base(name) {
 			this.day = day;
 		}
 
-		public FixedHoliday(string name, int month, int day)
+		public FixedHoliday(string name, int month, int day, Calendar calendar)
 			: base(name) {
 			this.day = new DayInYear (month, day);
 		}
 
+		public FixedHoliday(string name, int month, int day)
+			: this(name, month, day, new GregorianCalendar()) {
+		}
+
 		public override DateTime? GetInstance(int year) {
-			return new DateTime(year, day.Month, day.Day);
+			return day.GetDayOnYear(year);
 		}
 
 		public override bool IsInstanceOf(DateTime date) {
-			return date.Month == day.Month && date.Day == day.Day;
+			return day.IsTheSameDay(date);
 		}
 	}
 }
