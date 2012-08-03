1eeaddf6ed1da2f55893fe9581482f47a9165683
David Smith <db.ka.smtih@xtra.co.nz>
2012-08-03 15:12:11 +1200

    en-NZ holidays

---
+++ DateTimeExtensions.Tests/en-NZCalendarTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+using DateTimeExtensions.WorkingDays;
+using DateTimeExtensions.WorkingDays.CultureStrategies;
+
+
+namespace DateTimeExtensions.Tests
+{
+
+	[TestFixture]
+	public class en_NZCalendarTest
+	{
+
+		[Test]
+		public void DayAfterNewYearsDay()
+		{
+			var holiday = EN_NZHolidayStrategy.DayAfterNewYear;
+
+			var dateOnGregorian = new DateTime(2013, 1, 2);
+			TestHoliday(holiday, dateOnGregorian);
+
+			dateOnGregorian = new DateTime(2014, 1, 2); TestHoliday(holiday, dateOnGregorian);
+			dateOnGregorian = new DateTime(2015, 1, 2); TestHoliday(holiday, dateOnGregorian);
+			dateOnGregorian = new DateTime(2016, 1, 4); TestHoliday(holiday, dateOnGregorian);
+		}
+
+
+
+		private void TestHoliday(Holiday holiday, DateTime dateOnGregorian)
+		{
+			var holidayInstance = holiday.GetInstance(dateOnGregorian.Year);
+			Assert.IsTrue(holidayInstance.HasValue);
+			Assert.AreEqual(dateOnGregorian, holidayInstance.Value);
+			Assert.IsTrue(holiday.IsInstanceOf(dateOnGregorian));
+		}
+	}
+}
