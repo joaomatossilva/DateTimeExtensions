554f17831cf3c372899cfe4275602e83d2026d8c
scoutboy <seba00767@gmail.com>
2017-06-11 16:18:42 +0200

    Added Polish (pl_PL) strategy

---
+++ src/DateTimeExtensions/NaturalText/CultureStrategies/PL_PLNaturalTimeStrategy.cs
@@ -0,0 +1,59 @@
+﻿using DateTimeExtensions.Common;
+
+namespace DateTimeExtensions.NaturalText.CultureStrategies
+{
+    [Locale("pl-PL")]
+    public class PL_PLNaturalTimeStrategy : NaturalTimeStrategyBase
+    {
+        protected override string YearText
+        {
+            get { return "rok"; }
+        }
+
+        protected override string MonthText
+        {
+            get { return "miesiąc"; }
+        }
+
+        protected override string DayText
+        {
+            get { return "dzień"; }
+        }
+
+        protected override string HourText
+        {
+            get { return "godzina"; }
+        }
+
+        protected override string MinuteText
+        {
+            get { return "minuta"; }
+        }
+
+        protected override string SecondText
+        {
+            get { return "sekunda"; }
+        }
+
+        protected override string Pluralize(string text)
+        {
+            if (text.Equals("rok", StringComparison.OrdinalIgnoreCase))
+            {
+                return "lata";
+            }
+            if (text.Equals("miesiąc", StringComparison.OrdinalIgnoreCase))
+            {
+                return "miesiące";
+            }
+            if (text.Equals("dzień", StringComparison.OrdinalIgnoreCase))
+            {
+                return "dni";
+            }
+            if (text.EndsWith("a"))
+            {
+                return text.Remove(text.Length - 1) + "y";
+            }
+            return base.Pluralize(text);
+        }
+    }
+}
---
+++ src/DateTimeExtensions/WorkingDays/CultureStrategies/PL_PLHolidayStrategy.cs
@@ -0,0 +1,59 @@
+﻿using DateTimeExtensions.Common;
+
+namespace DateTimeExtensions.WorkingDays.CultureStrategies
+{
+    [Locale("pl-PL")]
+    public class PL_PLHolidayStrategy : HolidayStrategyBase, IHolidayStrategy
+    {
+        public PL_PLHolidayStrategy()
+        {
+            this.InnerHolidays.Add(GlobalHolidays.NewYear);
+
+            this.InnerHolidays.Add(ChristianHolidays.Epiphany);
+            this.InnerHolidays.Add(ChristianHolidays.Easter);
+            this.InnerHolidays.Add(ChristianHolidays.EasterMonday);
+
+            this.InnerHolidays.Add(GlobalHolidays.InternationalWorkersDay);
+
+            this.InnerHolidays.Add(May3rdConstitutionDay);
+
+            this.InnerHolidays.Add(ChristianHolidays.Pentecost);
+            this.InnerHolidays.Add(ChristianHolidays.CorpusChristi);
+            this.InnerHolidays.Add(ChristianHolidays.Assumption);
+            this.InnerHolidays.Add(ChristianHolidays.AllSaints);
+
+            this.InnerHolidays.Add(NationalIndependenceDay);
+
+            this.InnerHolidays.Add(ChristianHolidays.Christmas);
+            this.InnerHolidays.Add(ChristianHolidays.StStephansDay);
+        }
+
+        private static Holiday may3rdConstitutionDay;
+
+        public static Holiday May3rdConstitutionDay
+        {
+            get
+            {
+                if (may3rdConstitutionDay == null)
+                {
+                    may3rdConstitutionDay = new FixedHoliday("May 3rd Constitution Day", 3, 5);
+                }
+                return may3rdConstitutionDay;
+            }
+        }
+
+        private static Holiday nationalIndependenceDay;
+
+        public static Holiday NationalIndependenceDay
+        {
+            get
+            {
+                if (nationalIndependenceDay == null)
+                {
+                    nationalIndependenceDay = new FixedHoliday("National Independence Day", 11, 11);
+                }
+                return nationalIndependenceDay;
+            }
+        }
+    }
+}
