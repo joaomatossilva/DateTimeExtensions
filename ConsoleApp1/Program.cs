78ce5bb946cb037062ef599ce0932c3ca81a7dd3
Onur Özten <onurozten@alsay.com.tr>
2021-04-24 14:56:39 +0300

    Turkish (tr-TR) implamatation

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DateTimeExtensions;
+using DateTimeExtensions.WorkingDays;
+using DateTimeExtensions.WorkingDays.CultureStrategies;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = DateTime.Now.IsWorkingDay(new WorkingDayCultureInfo("tr-TR"));
+            var n1=new DateTime(2022,5,19).IsHoliday(new WorkingDayCultureInfo("tr-TR"));
+
+            var tatil=new TurkishHolidayStrategy().Holidays;
+
+            foreach (var holiday in tatil)
+            {
+                var k = holiday;
+                var kk = holiday.GetInstance(2030);
+                var d = kk.Value.DayOfWeek;
+                var endOfSubat = DateTime.Now;
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
