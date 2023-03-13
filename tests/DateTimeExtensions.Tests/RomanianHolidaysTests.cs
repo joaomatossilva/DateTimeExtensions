2de2ecb7a58b7e7b2e9b8c86b20a18bcac3143ca
totpero <www.totpe.ro@gmail.com>
2023-03-13 16:35:48 +0200

    new unit tests

--- tests/DateTimeExtensions.Tests/RomanianHolidaysTests.cs
+++ tests/DateTimeExtensions.Tests/RomanianHolidaysTests.cs
@@ -20,7 +20,7 @@ namespace DateTimeExtensions.Tests
         public void Romanian_has_12_main_holidays()
         {
             var holidays = dateTimeCulture.Holidays;
-            Assert.AreEqual(12, holidays.Count());
+            Assert.AreEqual(16, holidays.Count());
         }
 
         [Test]
@@ -32,7 +32,7 @@ namespace DateTimeExtensions.Tests
         [Test]
         public void Easter2018_is_1_april()
         {
-            Assert.That(new DateTime(2018, 4, 1).IsHoliday(dateTimeCulture));
+            Assert.That(new DateTime(2018, 4, 8).IsHoliday(dateTimeCulture));
         }
 
         [Test]
@@ -57,6 +57,9 @@ namespace DateTimeExtensions.Tests
         public void test_export()
         {
             var orthodoxEaster = ChristianOrthodoxHolidays.Easter;
+            var easter_1800 = orthodoxEaster.GetInstance(1800);
+            var easter_2017 = orthodoxEaster.GetInstance(2017);
+            var easter_2018 = orthodoxEaster.GetInstance(2018);
             var easter_2022 = orthodoxEaster.GetInstance(2022);
             var easter_2023 = orthodoxEaster.GetInstance(2023);
             var easter_2024 = orthodoxEaster.GetInstance(2024);
@@ -69,32 +72,31 @@ namespace DateTimeExtensions.Tests
             Assert.NotNull(s);
         }
 
-        //[Test]
-        //public void can_calculate_orthodox_easter()
-        //{
-        //    var years = new int[] { 1800, 1899, 1900, 1999, 2000, 2025, 2035, 2067, 2099 };
-        //    var easterDates = new Dictionary<int, DateTime>
-        //    {
-        //        {1800, new DateTime(1800, 4, 13)},
-        //        {1899, new DateTime(1899, 4, 2)},
-        //        {1900, new DateTime(1900, 4, 15)},
-        //        {1999, new DateTime(1999, 4, 4)},
-        //        {2000, new DateTime(2000, 4, 23)},
-        //        {2025, new DateTime(2025, 4, 20)},
-        //        {2035, new DateTime(2035, 3, 25)},
-        //        {2067, new DateTime(2067, 4, 3)},
-        //        {2099, new DateTime(2099, 4, 12)}
-        //    };
+        [Test]
+        public void can_calculate_orthodox_easter()
+        {
+            var years = new int[] { 1800, 1899, 1900, 1999, 2000, 2025, 2035, 2067, 2099 };
+            var easterDates = new Dictionary<int, DateTime>
+            {
+                {1800, new DateTime(1800, 4, 21)},
+                {1899, new DateTime(1899, 5, 1)},
+                {1900, new DateTime(1900, 4, 22)},
+                {1999, new DateTime(1999, 4, 11)},
+                {2000, new DateTime(2000, 4, 30)},
+                {2025, new DateTime(2025, 4, 20)},
+                {2035, new DateTime(2035, 4, 29)},
+                {2067, new DateTime(2067, 4, 10)},
+                {2099, new DateTime(2099, 4, 12)}
+            };
 
-        //    foreach (int year in years)
-        //    {
-        //        var easterHoliday = ChristianOrthodoxHolidays.Easter;
-        //        var easter = easterHoliday.GetInstance(year);
-        //        Assert.IsTrue(easter.HasValue);
-        //        Assert.AreEqual(DayOfWeek.Sunday, easter.Value.DayOfWeek);
-        //        Assert.AreEqual(easterDates[year], easter.Value);
-        //    }
-        //}
+            foreach (int year in years)
+            {
+                var easterHoliday = ChristianOrthodoxHolidays.Easter;
+                var easter = easterHoliday.GetInstance(year);
+                Assert.IsTrue(easter.HasValue);
+                Assert.AreEqual(easterDates[year], easter.Value);
+            }
+        }
 
         private void TestHoliday(Holiday holiday, DateTime dateOnGregorian)
         {
