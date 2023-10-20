306f3d4b1664c290f4ad350c26b4cebb4a686df1
JohanOsinga <johan@denow.nl>
2023-10-20 11:37:02 +0000

    Fix unit test for en-JM culture

--- tests/DateTimeExtensions.Tests/enJMHolidaysTests.cs
+++ tests/DateTimeExtensions.Tests/enJMHolidaysTests.cs
@@ -8,17 +8,34 @@ namespace DateTimeExtensions.Tests
     internal class enJMHolidaysTests
     {
         private readonly WorkingDayCultureInfo dateTimeCulture = new WorkingDayCultureInfo("en-JM", "Jamaica");
+
         [Test]
-        public void SundayChristmass2022()
-        {//Holiday's falling on Sunday are observed on the following monday.
-            //boxing day was the 26th, a monday, which would result in a clash, hence the clashing holiday would be observed on the following day (Tuesday)
-            var date = new DateTime(2022, 12, 25);
-            TestHoliday(dateTimeCulture, date);
-        }
-        private void TestHoliday(IWorkingDayCultureInfo workingDayCultureInfo, DateTime dateOnGregorian)
+        public void BoxingDay_2022_is_observed_tuesday()
         {
-            var isHoliday = workingDayCultureInfo.IsHoliday(dateOnGregorian);
-            Assert.IsTrue(isHoliday);
+            //Holiday's falling on Sunday are observed on the following monday.
+            //boxing day was the 26th, a monday, which would result in a clash, hence the clashing holiday would be observed on the following day (Tuesday)
+            var boxingDay = new DateTime(2022, 12, 26);
+            var boxingDayObserved = boxingDay.AddDays(1);
+            Assert.IsTrue(boxingDayObserved.IsHoliday(dateTimeCulture));
+        }
+
+        [Test]
+        public void Christmas_2022_sunday_is_observed_monday()
+        {
+            // Holiday's falling on Sunday are observed on the following monday.
+            // Christmas on a sunday should not be a holiday.
+            var christmasSunday = new DateTime(2022, 12, 25);
+            var christmasObserved = christmasSunday.AddDays(1);
+            Assert.IsTrue(christmasObserved.IsHoliday(dateTimeCulture));
+        }
+
+        [Test]
+        public void Christmas_sunday_not_a_holiday()
+        {
+            // Holiday's falling on Sunday are observed on the following monday.
+            // Christmas on a sunday should not be a holiday.
+            var christmasSunday = new DateTime(2022, 12, 25);
+            Assert.IsFalse(christmasSunday.IsHoliday(dateTimeCulture));
         }
     }
 }
