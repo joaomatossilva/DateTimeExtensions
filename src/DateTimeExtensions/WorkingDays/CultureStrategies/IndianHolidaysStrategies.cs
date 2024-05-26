0fa3af81f8d51df8c7308a6f918435c3c87e2205
AnirudhG07 <anirudhgupta@iisc.ac.in>
2024-05-26 23:55:41 +0530

    added updated IndianHolidaysStrategies.cs
    Removed previously present one with less holidays and added more
    holidays which inclue National and fixed festive ones.

--- src/DateTimeExtensions/WorkingDays/CultureStrategies/IndianHolidaysStrategies.cs
+++ src/DateTimeExtensions/WorkingDays/CultureStrategies/IndianHolidaysStrategies.cs
@@ -1,7 +1,3 @@
-#region License
-
-#endregion
-
 using System.Collections.Generic;
 using System.Globalization;
 using DateTimeExtensions.Common;
@@ -20,8 +16,13 @@ namespace DateTimeExtensions.WorkingDays.CultureStrategies
             this.InnerHolidays.Add(IndependenceDay);
             this.InnerHolidays.Add(GandhiJayanti);
             this.InnerHolidays.Add(Diwali);
-            this.InnerHolidays.Add(Holi);
-            this.InnerHolidays.Add(Janmashtami);
+            this.InnerHolidays.Add(Dussehra);
+            this.InnerHolidays.Add(GuruNanakJayanti);
+            this.InnerHolidays.Add(BuddhaPurnima);
+            this.InnerHolidays.Add(GoodFriday);
+            this.InnerHolidays.Add(ChristmasDay);
+            this.InnerHolidays.Add(EidUlFitr);
+            this.InnerHolidays.Add(EidUlAdha);
         }
 
         // Republic Day
@@ -89,52 +90,155 @@ namespace DateTimeExtensions.WorkingDays.CultureStrategies
             }
         }
 
-        // Holi (Variable every year)
-        private static Holiday holi;
-        public static Holiday Holi
+        // Dussehra (Vijayadashami) (Variable every year)
+        private static Holiday dussehra;
+        public static Holiday Dussehra
         {
             get
             {
-                if (holi == null)
+                if (dussehra == null)
                 {
-                    // Define the occurrences of Holi based on the Indian lunar calendar
-                    var knownHoliOccurences = new Dictionary<int, DayInYear>
+                    // Define the occurrences of Dussehra based on the Indian lunar calendar
+                    var knownDussehraOccurences = new Dictionary<int, DayInYear>
                     {
-                        {2021, new DayInYear(3, 29)},
-                        {2022, new DayInYear(3, 18)},
-                        {2023, new DayInYear(3, 8)},
-                        {2024, new DayInYear(3, 25)},
-
+                        {2022, new DayInYear(10, 5)},
+                        {2023, new DayInYear(10, 24)},
+                        {2024, new DayInYear(10, 12)},
+                        {2025, new DayInYear(10, 2)},
                     };
 
-                    holi = new YearMapHoliday("Holi", knownHoliOccurences);
+                    dussehra = new YearMapHoliday("Dussehra", knownDussehraOccurences);
                 }
-                return holi;
+                return dussehra;
             }
         }
 
-        // Janmashtami (Variable every year)
-        private static Holiday janmashtami;
-        public static Holiday Janmashtami
+        // Guru Nanak Jayanti (Variable every year)
+        private static Holiday guruNanakJayanti;
+        public static Holiday GuruNanakJayanti
         {
             get
             {
-                if (janmashtami == null)
+                if (guruNanakJayanti == null)
                 {
-                    // Define the occurrences of Janmashtami based on the Indian lunar calendar
-                    var knownJanmashtamiOccurences = new Dictionary<int, DayInYear>
-                    {    
-                        {2022, new DayInYear(8, 19)},
-                        {2023, new DayInYear(9, 7)},
-                        {2024, new DayInYear(8, 26)},
-                        {2025, new DayInYear(8, 16)},
-                       
+                    // Define the occurrences of Guru Nanak Jayanti based on the Indian lunar calendar
+                    var knownGuruNanakJayantiOccurences = new Dictionary<int, DayInYear>
+                    {
+                        {2022, new DayInYear(11, 8)},
+                        {2023, new DayInYear(11, 27)},
+                        {2024, new DayInYear(11, 15)},
+                        {2025, new DayInYear(11, 5)},
                     };
 
-                    janmashtami = new YearMapHoliday("Janmashtami", knownJanmashtamiOccurences);
+                    guruNanakJayanti = new YearMapHoliday("Guru Nanak Jayanti", knownGuruNanakJayantiOccurences);
                 }
-                return janmashtami;
+                return guruNanakJayanti;
             }
         }
-    }
+
+        // Buddha Purnima (Variable every year)
+        private static Holiday buddhaPurnima;
+        public static Holiday BuddhaPurnima
+        {
+            get
+            {
+                if (buddhaPurnima == null)
+                {
+                    // Define the occurrences of Buddha Purnima based on the Indian lunar calendar
+                    var knownBuddhaPurnimaOccurences = new Dictionary<int, DayInYear>
+                    {
+                        {2022, new DayInYear(5, 16)},
+                        {2023, new DayInYear(5, 5)},
+                        {2024, new DayInYear(5, 23)},
+                        {2025, new DayInYear(5, 12)},
+                    };
+
+                    buddhaPurnima = new YearMapHoliday("Buddha Purnima", knownBuddhaPurnimaOccurences);
+                }
+                return buddhaPurnima;
+            }
+        }
+
+        // Good Friday (Variable every year)
+        private static Holiday goodFriday;
+        public static Holiday GoodFriday
+        {
+            get
+            {
+                if (goodFriday == null)
+                {
+                    // Define the occurrences of Good Friday based on the Indian lunar calendar
+                    var knownGoodFridayOccurences = new Dictionary<int, DayInYear>
+                    {
+                        {2022, new DayInYear(4, 15)},
+                        {2023, new DayInYear(4, 7)},
+                        {2024, new DayInYear(3, 29)},
+                        {2025, new DayInYear(4, 18)},
+                    };
+
+                    goodFriday = new YearMapHoliday("Good Friday", knownGoodFridayOccurences);
+                }
+                return goodFriday;
+            }
+        }
+
+        // Christmas Day (fixed every year)
+        private static Holiday christmasDay;
+        public static Holiday ChristmasDay
+        {
+            get
+            {
+                if (christmasDay == null)
+                {
+                    christmasDay = new FixedHoliday("Christmas Day", 12, 25, IndianCalendar);
+                }
+                return christmasDay;
+            }
+        }
+
+        // Eid ul-Fitr (Variable every year)
+        private static Holiday eidUlFitr;
+        public static Holiday EidUlFitr
+        
+            get
+            {
+                if (eidUlFitr == null)
+                {
+                    // Define the occurrences of Eid ul-Fitr based on the Indian lunar calendar
+                    var knownEidUlFitrOccurences = new Dictionary<int, DayInYear>
+                    {
+                        {2022, new DayInYear(5, 2)},
+                        {2023, new DayInYear(4, 21)},
+                        {2024, new DayInYear(4, 10)},
+                        {2025, new DayInYear(3, 30)},
+                    };
+
+                    eidUlFitr = new YearMapHoliday("Eid ul-Fitr", knownEidUlFitrOccurences);
+                }
+                return eidUlFitr;
+            }
+        }
+
+        // Eid ul-Adha (Variable every year)
+        private static Holiday eidUlAdha;
+        public static Holiday EidUlAdha
+        {
+            get
+            {
+                if (eidUlAdha == null)
+                {
+                    // Define the occurrences of Eid ul-Adha based on the Indian lunar calendar
+                    var knownEidUlAdhaOccurences = new Dictionary<int, DayInYear>
+                    {
+                        {2022, new DayInYear(7, 10)},
+                        {2023, new DayInYear(6, 29)},
+                        {2024, new DayInYear(6, 17)},
+                        {2025, new DayInYear(6, 6)},
+                    };
+
+                    eidUlAdha = new YearMapHoliday("Eid ul-Adha", knownEidUlAdhaOccurences);
+                }
+                return eidUlAdha;
+            }
+        }
 }
