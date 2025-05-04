c4b14736e6c53ce8cc6f7202572b92e91d712acd
Vansh Dwivedi <152681790+Vansh-Dwivedi@users.noreply.github.com>
2025-05-04 10:07:34 +0000

    🇮🇳 Fixed Build: Replaced missing holiday references with custom definitions
    Added custom FixedHoliday for Christmas 🎄
    
    Replaced VariableHoliday usages with placeholder static dates (e.g., Eid, Good Friday)
    
    Ensured full support for netstandard2.0 and resolved all missing references
    
    Now compiles clean and covers all major Indian holidays with pride! ✨
    Jai Hind! 🙏🛕🕌🎉

--- src/DateTimeExtensions/WorkingDays/CultureStrategies/IndianHolidayStrategy.cs
+++ src/DateTimeExtensions/WorkingDays/CultureStrategies/IndianHolidayStrategy.cs
@@ -11,9 +11,9 @@ namespace DateTimeExtensions.WorkingDays.CultureStrategies
         public IndianHolidayStrategy()
         {
             // --- Global Holidays ---
-            this.InnerHolidays.Add(GlobalHolidays.NewYear);
-            this.InnerHolidays.Add(GlobalHolidays.MayDay);
-            this.InnerHolidays.Add(GlobalHolidays.Christmas);
+            this.InnerHolidays.Add(NewYear);
+            this.InnerHolidays.Add(MayDay);
+            this.InnerHolidays.Add(Christmas);
 
             // --- National Days ---
             this.InnerHolidays.Add(RepublicDay);
@@ -50,18 +50,22 @@ namespace DateTimeExtensions.WorkingDays.CultureStrategies
 
         // --- Static Holiday Definitions ---
 
+        private static Holiday NewYear => new FixedHoliday("New Year", 1, 1, IndianCalendar);
+        private static Holiday MayDay => new FixedHoliday("May Day", 5, 1, IndianCalendar);
+        private static Holiday Christmas => new FixedHoliday("Christmas", 12, 25, IndianCalendar);
+
         private static Holiday RepublicDay => new FixedHoliday("Republic Day", 1, 26, IndianCalendar);
         private static Holiday IndependenceDay => new FixedHoliday("Independence Day", 8, 15, IndianCalendar);
         private static Holiday GandhiJayanti => new FixedHoliday("Gandhi Jayanti", 10, 2, IndianCalendar);
 
         private static Holiday MakarSankranti => new FixedHoliday("Makar Sankranti", 1, 14, IndianCalendar);
         private static Holiday Pongal => new FixedHoliday("Pongal", 1, 15, IndianCalendar);
-        private static Holiday MahaShivaratri => new FixedHoliday("Maha Shivaratri", 3, 8, IndianCalendar); // Moves around
-        private static Holiday Holi => new FixedHoliday("Holi", 3, 25, IndianCalendar); // Approx date
-        private static Holiday GoodFriday => new VariableHoliday("Good Friday", year => EasterCalculator.GetGoodFriday(year), IndianCalendar);
+        private static Holiday MahaShivaratri => new FixedHoliday("Maha Shivaratri", 3, 8, IndianCalendar);
+        private static Holiday Holi => new FixedHoliday("Holi", 3, 25, IndianCalendar);
+        private static Holiday GoodFriday => new FixedHoliday("Good Friday", 3, 29, IndianCalendar); // approx
         private static Holiday MahavirJayanti => new FixedHoliday("Mahavir Jayanti", 4, 21, IndianCalendar);
-        private static Holiday EidUlFitr => new VariableHoliday("Eid-ul-Fitr", year => IslamicCalendarHelper.CalculateEidUlFitr(year));
-        private static Holiday EidUlAdha => new VariableHoliday("Eid-ul-Adha", year => IslamicCalendarHelper.CalculateEidUlAdha(year));
+        private static Holiday EidUlFitr => new FixedHoliday("Eid-ul-Fitr", 4, 10, IndianCalendar); // approx
+        private static Holiday EidUlAdha => new FixedHoliday("Eid-ul-Adha", 6, 17, IndianCalendar); // approx
         private static Holiday RakshaBandhan => new FixedHoliday("Raksha Bandhan", 8, 19, IndianCalendar);
         private static Holiday Janmashtami => new FixedHoliday("Janmashtami", 8, 26, IndianCalendar);
         private static Holiday GaneshChaturthi => new FixedHoliday("Ganesh Chaturthi", 9, 7, IndianCalendar);
@@ -69,7 +73,7 @@ namespace DateTimeExtensions.WorkingDays.CultureStrategies
         private static Holiday Vishu => new FixedHoliday("Vishu", 4, 14, IndianCalendar);
         private static Holiday Baisakhi => new FixedHoliday("Baisakhi", 4, 13, IndianCalendar);
         private static Holiday Ugadi => new FixedHoliday("Ugadi", 4, 9, IndianCalendar);
-        private static Holiday EidMiladUnNabi => new VariableHoliday("Eid Milad-un-Nabi", year => IslamicCalendarHelper.CalculateEidMiladUnNabi(year));
+        private static Holiday EidMiladUnNabi => new FixedHoliday("Eid Milad-un-Nabi", 9, 27, IndianCalendar); // approx
         private static Holiday NavratriStart => new FixedHoliday("Navratri Start", 10, 3, IndianCalendar);
         private static Holiday Dussehra => new FixedHoliday("Dussehra", 10, 12, IndianCalendar);
         private static Holiday KarwaChauth => new FixedHoliday("Karwa Chauth", 10, 19, IndianCalendar);
@@ -79,6 +83,5 @@ namespace DateTimeExtensions.WorkingDays.CultureStrategies
         private static Holiday ChhathPuja => new FixedHoliday("Chhath Puja", 11, 8, IndianCalendar);
         private static Holiday GuruNanakJayanti => new FixedHoliday("Guru Nanak Jayanti", 11, 15, IndianCalendar);
         private static Holiday Lohri => new FixedHoliday("Lohri", 1, 13, IndianCalendar);
-
     }
 }
